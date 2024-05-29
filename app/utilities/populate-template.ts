3bff97631eca4ffe18437281dddd97fc4fe04c9b
Welldar <eldarzheryakov@gmail.com>
2024-05-30 01:04:10 +0500

    refac: split logic to small func

--- app/utilities/populate-template.ts
+++ app/utilities/populate-template.ts
@@ -1,94 +1,89 @@
 import { unzip } from 'unzipit'
 import { decodeHTML } from './decode-html'
-import { makeExportUrl } from './make-export-url'
 import { filterEmptyColumns, isNotEmptyRow } from './filter-table'
 import { getMainHTMLFile } from './get-main-file'
 import { proxySrc } from './proxy-src-attr'
+import { getTablesDOM } from './get-tables-dom'
 
 export async function populateTemplateHTML(
-  tableUserUrl: string,
-  templateUserUrl: string
+  tableUrl: string,
+  templateUrl: string
 ) {
-  const [tableUrl, templateUrl] = [
-    makeExportUrl(tableUserUrl),
-    makeExportUrl(templateUserUrl),
-  ]
-
-  if (!tableUrl || !templateUrl) return
-
-  const [{ entries: entriesTable }, { entries: entriesTemplate }] =
+  const [{ entries: entriesTables }, { entries: entriesTemplate }] =
     await Promise.all([unzip(tableUrl), unzip(templateUrl)])
+  const templateBlob = getMainHTMLFile(entriesTemplate)
 
-  const templateEntry = getMainHTMLFile(entriesTemplate)
+  if (!templateBlob) return
 
-  if (!templateEntry) return
-
-  const templateDoc = decodeHTML(await templateEntry.text())
-  const placeholders = [...templateDoc.matchAll(/{[^{}]+(?:-\d+){2,4}}/gi)]
+  const templateHTML = decodeHTML(await templateBlob.text())
+  const placeholders = [...templateHTML.matchAll(/{[^{}]+(?:-\d+){2,4}}/gi)]
   const placeholderParams = placeholders.map((regExpExec) =>
     regExpExec[0].slice(1, -1).split('-')
   )
-  const tablesPending = new Map<string, Promise<string>>()
-
-  placeholderParams.forEach((params) => {
-    const [filename] = params
-
-    if (tablesPending.has(filename)) return
-
-    tablesPending.set(filename, entriesTable[filename + '.html'].text())
-  })
-
-  const tablesDOM = new Map(
-    await Promise.all(
-      [...tablesPending.entries()].map(async (tableRecord) => {
-        const [filename, tablePromise] = tableRecord
-        const tableHTML = await tablePromise
-        const parser = new DOMParser()
-        const document = parser.parseFromString(tableHTML, 'text/html')
-
-        return Promise.all([filename, document])
-      })
-    )
+  const tablesDOM = await getTablesDOM(
+    placeholderParams.map(([filename]) => filename),
+    entriesTables
   )
 
-  return placeholderParams.reduce((template, params, ind) => {
-    let [filename, column1, row1, column2, row2] = params
+  return placeholderParams.reduce(function replacePlaceholders(
+    template,
+    params,
+    ind
+  ) {
+    const [filename, column1, row1] = params
+    const column2 = params[3] ?? column1
+    const row2 = params[4] ?? row1
+    const tbody = tablesDOM.get(filename)?.querySelector('tbody')
 
-    column2 = column2 ? column2 : column1
-    row2 = row2 ? row2 : row1
+    if (!tbody) throw new Error('Probably not correct table')
 
-    let rows = [...tablesDOM.get(filename)!.querySelectorAll('tr')]
-      .slice(+row1, +row2 + 1)
+    const rows = getQueriedRange(tbody, 'tr', row1, row2)
+      .map((row) => row.cloneNode(true) as HTMLTableRowElement)
       .map((row) => {
-        const newRow = row.cloneNode(true) as HTMLTableRowElement
-        newRow.replaceChildren(
-          ...[...row.querySelectorAll('td')].slice(
-            // rewrite to css selector nth-child
-            +column1 - 1,
-            +column2
-          )
+        const cells = getQueriedRange(row, 'td', column1, column2).map(
+          (cell) => cell.cloneNode(true) as HTMLTableCellElement
         )
 
-        return newRow
+        row.replaceChildren(...cells)
+
+        return row
       })
       .filter(isNotEmptyRow)
 
-    rows = filterEmptyColumns(rows)
-    proxySrc(rows)
+    const table = document.createElement('table')
+    table.append(...rows)
+
+    filterEmptyColumns(table)
+    table.querySelectorAll('[src]').forEach(proxySrc)
 
     const placeholder = placeholders[ind][0]
+    const value = getReplaceValue(table) ?? ''
 
-    let variable = ''
+    return template.replaceAll(placeholder, value)
+  }, templateHTML)
+}
 
-    if (rows.length == 1) {
-      variable = rows[0].innerHTML
-    } else if (rows.length > 1) {
-      const table = document.createElement('table')
+function getReplaceValue(table: HTMLTableElement) {
+  if (table.children.length == 1) {
+    return table.children.item(0)!.innerHTML
+  } else if (table.children.length > 1) {
+    const div = document.createElement('div')
 
-      table.append(...rows)
-      variable = table.outerHTML
-    }
+    div.className = 'table-wrapper'
+    div.append(table)
+    return div.outerHTML
+  }
+}
 
-    return template.replaceAll(placeholder, variable)
-  }, templateDoc)
+function getQueriedRange(
+  root: Element,
+  type: string,
+  start: string,
+  end: string
+) {
+  return [
+    ...root.querySelectorAll(
+      `${type}:nth-of-type(n + ${start}):nth-of-type(-n + ${end})`
+    ),
+  ]
 }
