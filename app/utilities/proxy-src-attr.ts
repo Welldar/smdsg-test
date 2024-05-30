38a663679b579f5603b42d032396baf55105ae95
Welldar <eldarzheryakov@gmail.com>
2024-05-30 17:47:03 +0500

    fix: img display in .docx

--- app/utilities/proxy-src-attr.ts
+++ app/utilities/proxy-src-attr.ts
@@ -6,6 +6,9 @@ export function proxySrc(media: Element) {
   const searchParams = new URLSearchParams({ url: src })
 
   // had to make it an absolute url to make img work in .docx
-  media.setAttribute('src', `/api/proxy?${searchParams}`)
+  media.setAttribute(
+    'src',
+    `https://smdsg-test.vercel.app/api/proxy?${searchParams}`
+  )
   media.setAttribute('crossorigin', 'anonymous')
 }
